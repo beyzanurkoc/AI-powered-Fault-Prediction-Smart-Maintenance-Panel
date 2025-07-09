766bae1e214f9c7a865935705cf0a979a8b2d5c5
Beyza Nur Koc <beyzannkoc@gmail.com>
2025-07-09 13:50:47 +0300

    ML part is written

---
+++ model_evaluation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+
+df = pd.read_csv("sample_data.csv")
+
+X = df[["temperature", "pressure", "engine_rpm"]]
+y = df["code"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+# sample input !!!
+sample_input = pd.DataFrame({"temperature": [125], "pressure": [2.7], "engine_rpm": [1350]})
+predicted_code = model.predict(sample_input)
+print("\nPredicted fault code for input sample:", predicted_code[0])
+
+probas = model.predict_proba(sample_input)
+top_n_idx = probas[0].argsort()[-3:][::-1]
+top_codes = model.classes_[top_n_idx]
+print("\nMost probable fault codes (top 3):", top_codes)
+
+
+cm = confusion_matrix(y_test, y_pred, labels=model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+disp.plot(xticks_rotation=90)
+plt.show()
