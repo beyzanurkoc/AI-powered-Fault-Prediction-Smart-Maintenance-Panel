ab32904b642cda094888965056afd16fd316b236
Beyza Nur Koc <beyzannkoc@gmail.com>
2025-07-09 16:38:19 +0300

    update

--- read_data.py
+++ read_data.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-# Change the file path to your CSV file's actual location if needed
 file_path = "data_clean.csv"  #fault code & description 
 
-# Read the CSV file
 df = pd.read_csv(file_path)
 
-# Show all rows 
-print(df)
+print(df) 
 
